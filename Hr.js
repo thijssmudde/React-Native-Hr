ce5a27006534de07809d02035c2a3588d4839afb
fullhdpixel <tsmudde@hotmail.com>
2018-03-12 12:28:35 +0100

    1.0.4

--- Hr.js
+++ Hr.js
@@ -1,35 +1,38 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-//Styles
-import styles from "./HrStyles";
-//Validation
-import PropTypes from "prop-types";
+import React from 'react'
+import {View} from 'react-native'
+import PropTypes from 'prop-types'
 
-export default class Hr extends Component {
+import styled from 'styled-components'
+
+const Row = styled.View `
+  flexDirection: row;
+`
+
+const Side = styled.View `
+  height: this.props.width;
+  backgroundColor: ${props => props.color
+  ? props.color
+  : "white"};
+  height: ${props => props.width
+    ? props.width
+    : 1};
+  flex: 1;
+  alignSelf: center;
+`
+
+export default class Hr extends React.Component {
   render() {
     return (
-      <View style={styles.row}>
-        <View
-          style={[
-            styles.side,
-            { height: this.props.width, backgroundColor: this.props.color }
-          ]}
-        />
-        {this.props.children}
-        <View
-          style={[
-            styles.side,
-            { height: this.props.width, backgroundColor: this.props.color }
-          ]}
-        />
-      </View>
-    );
+      <Row>
+        <Side width={this.props.width} color={this.props.color}/>{this.props.children}
+        <Side width={this.props.width} color={this.props.color}/>
+      </Row>
+    )
   }
 }
 
-//Validate all props
 Hr.propTypes = {
   width: PropTypes.number,
-  color: PropTypes.color,
-  children: PropTypes.children
-};
+  color: PropTypes.string,
+  children: PropTypes.element.isRequired
+}
